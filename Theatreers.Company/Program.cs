09a860de6d1a3f1479ff3f15aa76c809bd5ca1bf
Christian Reddington <christian.reddington@microsoft.com>
2018-11-27 10:42:11 +0000

    Initialise AppInsights

--- Theatreers.Company/Program.cs
+++ Theatreers.Company/Program.cs
@@ -19,6 +19,7 @@ namespace Theatreers.Company
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseApplicationInsights(); // Enable Application Insights
     }
 }
